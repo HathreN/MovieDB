959576c8e62453a891c69377f5da6b0eae88652b
Kdabrowa6@gmail.com <5z4l3j@gmail.com>
2021-11-25 15:24:56 +0100

    Etap 3

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,7 @@ function Header() {
         return (
             <div id="header" className="header">
                 <div id="logo" className="logo"><img className="logoImage" src={logo} alt="Logo"></img></div>
-                <div id="searchBar" className="searchBar"><input className="searchBar" placeholder="Wpisz tytuł filmu"></input></div>
+                <div id="searchBar" className="searchBar"><input className="searchBar" placeholder="Enter movie title here"></input></div>
                 <div id="logIn" className="logIn"><img className="logInButton" src={logInButton} alt="my image" onClick={refresh}/></div>
             </div>);
 }
